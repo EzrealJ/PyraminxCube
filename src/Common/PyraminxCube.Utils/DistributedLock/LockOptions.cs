a78d0ec549551c2c39819e7f88c472cfb6c85ae4
Ezreal <memory_of_you@hotmail.com>
2025-05-02 14:10:30 +0800

    调整分布式锁定义

---
+++ src/Common/PyraminxCube.Utils/DistributedLock/LockOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyraminxCube.Utils.DistributedLock
+{
+    public class LockOptions
+    {
+        public LockOptions()
+        {
+        }
+
+        public LockOptions(string token, TimeSpan timeout)
+        {
+            Token = token;
+            Timeout = timeout;
+        }
+
+        public string Token { get; set; } = string.Empty;
+        public TimeSpan Timeout { get; set; }
+    }
+}
